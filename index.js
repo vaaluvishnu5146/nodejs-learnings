413c291db62f1d3b9099f608745fa9b593105d35
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-02-24 17:07:36 +0530

    Route to create file inside a folder

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(`<html>
+  const fileName = "message.txt";
+  const folder = "logs";
+  try {
+    fs.appendFileSync(`./${folder}/${fileName}`, "data to append");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(`<html>
             <head>
-            <title>Home</title>
+            <title>Result</title>
             </head>
             <body>
-            <h1>Home page</h1>
+            <h1>${fileName} creation inside ${folder} folder successful</h1>
             </body>
                 </html >`);
+  } catch (err) {
+    // Handle the error
+    console.log(err);
+  }
 });
 
 server.listen(port, hostname, () => {
