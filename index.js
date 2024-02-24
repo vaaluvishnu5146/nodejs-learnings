39f64b0459e5add41d556da310c7cef31da9e4d5
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-02-24 16:44:19 +0530

    process of sending json in response

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const http = require("node:http");
+const Products = require("./products.json");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(`<html>
-            <head>
-            <title>Home</title>
-            </head>
-            <body>
-            <h1>Home page</h1>
-            </body>
-                </html >`);
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(Products));
 });
 
 server.listen(port, hostname, () => {
